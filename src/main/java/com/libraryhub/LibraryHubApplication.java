1b7e3c1a457870bfcd8f627fa18a11a1c0957ed7
Francesco Giannulo <frangiannu@gmail.com>
2026-03-12 11:04:46 +0100

    feat: rename base package and define project structure

--- src/main/java/com/libraryhub/LibraryHubApplication.java
+++ src/main/java/com/libraryhub/LibraryHubApplication.java
@@ -1,4 +1,4 @@
-package com.libraryhub.libraryhub;
+package com.libraryhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
